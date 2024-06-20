621473248028aebfc43ab83bb62d695422c4a4e2
zhouyuanmin <1837722596@qq.com>
2024-06-20 11:05:09 +0800

    经纬度查询，给机场加上gps信息

--- tiger/spider_ctrip.py
+++ tiger/spider_ctrip.py
@@ -1,13 +1,21 @@
 import os
 import json
-
+import time
+from selenium.common.exceptions import UnexpectedAlertPresentException
 from tiger.spider import base_dir, logger, create_browser
 
 # 页面节点
-page_elements = {"airport_li": '//*[@id="content"]/div/div[2]/div/div/div/div/div/div/ul/li'}
+page_elements = {
+    "airport_li": '//*[@id="content"]/div/div[2]/div/div/div/div/div/div/ul/li',
+    "textbox": '//*[@id="sou_suo_value"]',
+    "button": '//*[@id="api_map_top"]/input[2]',
+    "lng_show": '//*[@id="all_lng_show"]',
+    "lat_show": '//*[@id="all_lat_show"]',
+}
 
 
 def get_airports(airports_file=""):
+    """爬取携程，获取基本的机场信息，有266个机场"""
     if not airports_file:
         airports_file = os.path.join(base_dir, "trash", "airports.json")
 
@@ -29,5 +37,51 @@ def get_airports(airports_file=""):
     logger.info(f"抓取完成，共{len(airports)}个机场")
 
 
+def add_gps(old_airports_file="", new_airports_file=""):
+    """经纬度查询，给机场加上gps信息"""
+    if not old_airports_file:
+        old_airports_file = os.path.join(base_dir, "resources", "airports.json")
+    if not new_airports_file:
+        new_airports_file = os.path.join(base_dir, "trash", "airports.json")
+
+    # 读取JSON文件的数据
+    with open(old_airports_file, "r") as file:
+        airports = json.load(file)
+
+    browser = create_browser()
+    url = "https://jingweidu.bmcx.com/"
+    browser.get(url)
+    for i, k in enumerate(airports, start=1):
+        if airports[k].get("lng", ""):
+            continue
+        try:
+            textbox = browser.find_element_by_xpath(page_elements.get("textbox"))
+            textbox.clear()
+            textbox.send_keys(k)
+            button = browser.find_element_by_xpath(page_elements.get("button"))
+            button.click()
+            time.sleep(10)
+            lng_show = browser.find_element_by_xpath(page_elements.get("lng_show"))
+            lng = lng_show.get_attribute("value")
+            lat_show = browser.find_element_by_xpath(page_elements.get("lat_show"))
+            lat = lat_show.get_attribute("value")
+            airports[k]["lng"] = lng
+            airports[k]["lat"] = lat
+            logger.info(f"{i}:{json.dumps(airports[k], ensure_ascii=False)}")
+        except UnexpectedAlertPresentException as e:
+            logger.warning(e)
+            time.sleep(660)
+            continue
+        except Exception as e:
+            logger.error(e)
+        finally:
+            with open(new_airports_file, "w") as f:
+                content = json.dumps(airports, ensure_ascii=False)
+                f.write(content)
+    browser.quit()
+    logger.info(f"抓取完成，共更新{len(airports)}个机场")
+
+
 if __name__ == "__main__":
-    get_airports()
+    # get_airports()
+    add_gps()
